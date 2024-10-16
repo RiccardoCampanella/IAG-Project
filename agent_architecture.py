fead66892b794b18ac7bc177803cb2e9a2e18df6
SquidwardInc <103361236+SquidwardInc@users.noreply.github.com>
2024-10-16 13:11:44 +0200

    update reasoning

--- agent_architecture.py
+++ agent_architecture.py
@@ -2,6 +2,22 @@ from owlready2 import *
 from pprint import pprint
 path = "C:\\Users\\tijme\\Documents\\GitHub\\IAG-Project\\intelligent_agents.rdf"
 
+from owlready2 import * 
+import owlready2
+owlready2.JAVA_EXE = r"C:\Users\lucmi\Downloads\Protege-5.6.4-win\Protege-5.6.4\jre\bin\java.exe"
+# Create a Graph \Documents\Opdrachten\IAG-Project
+onto = get_ontology(r"C:\Users\lucmi\Documents\Opdrachten\IAG-Project\intelligent_agents.rdf").load()
+#print(type(onto))
+#for entity in onto.entities():
+#    print(entity)
+with onto:
+    sync_reasoner()
+#for cls in onto.classes():
+ #   print("yeah", cls)
+ar1 = onto.Healthy.instances()
+ar2 = onto.Sport.instances()
+inte = list(set(ar1).intersection(ar2))
+print("result", inte)
 
 class goal_based_agent:
   def __init__(self, path="onto_pizza.owl"):
