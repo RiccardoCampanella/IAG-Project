614331ce323499be092136009dcac81afa49c054
Riccardo Campanella <cmprcr99d17c349s@unical.it>
2024-10-16 12:51:41 +0200

    add Agent, State, Goal classes

--- agent_architecture.py
+++ agent_architecture.py
@@ -1,12 +1,85 @@
-class goal_based_agent():
-  __init__():
+class AgentState:
+    IDLE = 0
+    INPUT_PROCESSING = 1
+    INFORMATION_GATHERING = 2
+    EVIDENCE_GATHERING = 3
+    REASONING = 4
+    RECOMMENDATION_FORMULATION = 5
+    OUTPUT_GENERATION = 6
+    SELF_EVALUATION = 7
 
-  function_make_query_from_nl():
+class Goal:
+    def __init__(self, description, applicable_states):
+        self.description = description
+        self.applicable_states = applicable_states
 
-  function_get_info_ontology():
+class FakeNewsAgent:
+  def __init__(self):
+      self.state = AgentState.IDLE
+      self.main_goal = "Perfrom fact-checking and provide return statement"
+      self.subgoal = [
+          Goal("Query the ontology to get evidences", AgentState.EVIDENCE_GATHERING),
+          Goal("Retrieve evidences from external source (LLM)", AgentState.EVIDENCE_GATHERING),
+          Goal("Compare and analyze the evidences", AgentState.REASONING),
+          Goal("Compute and analyze evidences", AgentState.REASONING),
+          Goal("Present results to the user")
+      ]
+      self.current_strategy = None
 
-  function_get_info_gpt():
+  def pursue_goal(self, news_item):
+      self.analyze_item(news_item)
+      while not self.goal_achieved():
+          self.select_strategy()
+          self.execute_strategy()
+          self.evaluate_progress()
+          self.adapt_approach()
+  
+  def analyze_item():
+      # code
+      return 
+  
+  def select_strategy():
+      # code
+      return 
+  
+  def execute_strategy():
+      # code
+      return 
+  
+  def evaluate_strategy():
+      # code 
+      return 
+  
+  def evaluate_progress():
+      # code
+      return 
+  
+  def adapt_approach():
+      # code
+      return 1
+  
+  def goal_achieved():
+      # code
+      return 
+  
+  ### Agent-KnowledgeBase interaction functions
 
-  function_rank_info():
+  def function_make_query_from_nl():
+      # code
+      return
 
-  function_make_reccomendation():
+  def function_get_info_ontology():
+      # code
+      return
+
+  def function_get_info_gpt():
+      # code
+      return 
+
+  def function_rank_info():
+      # code
+      return 
+
+  def function_make_reccomendation():
+      # code
+      return 
