766a1b87928edfe4c690258eaecd15d2e127cf94
SquidwardInc <103361236+SquidwardInc@users.noreply.github.com>
2024-10-22 15:38:38 +0200

    fix

--- agent_architecture.py
+++ agent_architecture.py
@@ -28,8 +28,74 @@ class goal_based_agent:
     #self.label_to_class = {ent.label[0]: ent for ent in self.ontology.classes()}
     #self.label_to_prop = {prop.label[0]: prop for prop in self.ontology.properties()}
 
+<<<<<<< Updated upstream
     self.class_to_label = {ent:ent.label for ent in self.ontology.classes()}
     #self.prop_to_label = {prop:prop.label[0] for prop in self.ontology.properties()}
+=======
+      llmQuery = strQuestion + \
+        " Present arguments concisely, " + \
+        "focusing on evidence without speculation, " + \
+        "and structure the response as evidence for or against the statement. " + \
+        "Please give every statement a score from 1-10 on how reliable it is." 
+      
+      self.lstLLMQueries.append(llmQuery)
+    print(self.lstLLMQueries)
+    self.boolGetLLMQueries = True
+  
+  def reasoning(self, arguments, target = None, forall = None):
+      instances_list = []
+      for datatype, argument in arguments:
+          if datatype == "class":
+              some_class = self.ontology.search_one(iri="*#" + argument)
+              instances_list.append([str(x) for x in some_class.instances()])
+          if datatype == "objectproperty":
+              sublist = self.searchproperties(argument[0], argument[1])
+              instances_list.append(sublist)
+      intersection = list(reduce(set.intersection, map(set, instances_list)))
+      if target is not None and target in intersection:
+          return True
+      if forall is not None:
+          if forall[0] == "class":
+              forallargument = self.ontology.search_one(iri="*#" + forall[1])
+              for i in intersection:
+                  if i not in forallargument.instances():
+                      return False
+              return True
+          if forall[0] == "objectproperty":
+              objectlist = self.searchproperties(forall[1][0], forall[1][1])
+              for i in intersection:
+                  if i not in objectlist:
+                      return False
+              return True
+  def searchproperties(self, prop, argument):
+      objectlist = []
+      ontoproperty = self.ontology.search_one(iri="*#" + prop)
+      ontoinstance = self.ontology.search_one(iri="*#" + argument)
+      for instance in self.ontology.individuals():
+          if ontoproperty in instance.get_properties():
+              for sub, obj in ontoproperty.get_relations():
+                  if sub == instance and obj == ontoinstance:
+                      objectlist.append(instance)
+      return objectlist
+  def get_LLM_arguments(self):
+    LLMresponse = self.LLM_query(self.lstLLMQueries[0])
+    self.lstArguments = self.extract_text(LLMresponse)
+    print(self.lstArguments)
+   
+  def extract_text(self, text): #Function by chatgpt to parse the query response.
+      # Split the text into lines
+      lines = text.split("\n")
+      
+      # Extract questions, removing additional text or brackets
+      questions = []
+      for line in lines:
+          # Check if line contains a question
+          if '. ' in line:
+              # Extract the question part before any bracketed text
+              question = re.sub(r"\s*\(.*?\)", "", line.split('. ', 1)[1]).strip()
+              questions.append(question)
+      return questions
+>>>>>>> Stashed changes
 
     pprint(type(list(self.ontology.classes())[0]))
     list(default_world.sparql("""
